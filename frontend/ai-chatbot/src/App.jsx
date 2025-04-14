61fb83a5aed221892705a8082cc7a5b414ca9124
Jinuka <sethjinuka@gmail.com>
2025-04-14 15:35:57 +0530

    second commit

--- frontend/ai-chatbot/src/App.jsx
+++ frontend/ai-chatbot/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
 import React from 'react'
+import Chat from './components/Chat'
+
 function App() {
   
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <main>
+      <Chat />
+    </main>
   )
 }
 
